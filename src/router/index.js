a640f8540acb1b5a6bc5eff9ebed82eafb20ad03
zayarmyooo19 <zayarmyooo1832003@gmail.com>
2023-06-03 19:30:47 +0630

    router guard complete

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
 import Chatroom from '../views/Chatroom.vue'
+import { auth } from '../firebase/config'
 
 const routes = [
   {
@@ -11,7 +12,16 @@ const routes = [
   {
     path: '/chatroom',
     name: 'Chatroom',
-    component: Chatroom
+    component: Chatroom,
+    beforeEnter:(to,from,next)=>{
+      let user=auth.currentUser
+      if(user){
+        next();
+      }else{
+        next({name:"Welcome"});
+      }
+
+    }
 
   },
 ]
